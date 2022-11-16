754160052012b2b28e32a2a58fe818f2c8c6d197
L2undercover <lwessecopela2022@elam.co.uk>
2022-11-16 11:53:05 +0000

    Cursor lock

--- Assets/Scenes/Scripts/MovingPlatform.cs
+++ Assets/Scenes/Scripts/MovingPlatform.cs
@@ -11,6 +11,7 @@ public class MovingPlatform : MonoBehaviour
     public float speed;
 
     bool isReversing = false;
+
     void Start()
     {
         myPlatform.position = myStartPoint.position;
@@ -22,10 +23,19 @@ public class MovingPlatform : MonoBehaviour
         if (isReversing == false)
         {
             myPlatform.position = Vector3.MoveTowards(myPlatform.position, myEndPoint.position, speed);
-        }
-        else
+
+            if (myPlatform.position == myEndPoint.position)
+            {
+                isReversing = true;
+            }
+        } else
         {
             myPlatform.position = Vector3.MoveTowards(myPlatform.position, myStartPoint.position, speed);
+
+            if (myPlatform.position == myStartPoint.position)
+            {
+                isReversing = false;
+            }
         }
     }
 }
